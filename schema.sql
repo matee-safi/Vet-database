20725d5f281b98dfc2447a69f5dfb2756774cde7
matee-safi <matiullahsafibbc@gmail.com>
2023-05-11 01:57:46 +0430

    git revert

--- schema.sql
+++ schema.sql
@@ -3,19 +3,3 @@
 CREATE TABLE animals (
     name varchar(100)
 );
-
-create database vet_clinic;
-
-\c vet_clinic;
-
-CREATE TABLE animals (
-    id int,
-    name varchar(40),
-    date_of_birth date,
-    escape_attempts int,
-    neutered boolean,
-    weight_kg decimal
-);
-
-insert into animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
-values ('Agumon', '2020-2-3', 10.23, true, 0);
