bcaf7e40d171141adce46af5af38eff8ae45ab54
matee-safi <matiullahsafibbc@gmail.com>
2023-05-13 05:16:06 +0430

    Add new tables

--- data.sql
+++ data.sql
@@ -14,3 +14,47 @@ values ('Charmander', '2020-2-8', 11, false, 0),
 ('Boarmon', '2005-6-7', 20.4, true, 7),
 ('Blossom', '1998-10-13', 17, true, 3),
 ('Ditto', '2022-5-14', 22, true, 4);
+
+insert into owners (id, full_name, age)
+values(1, 'Sam Smith', 34),
+(2, 'Jennifer Orwell', 19),
+(3, 'Bob', 45),
+(4, 'Melody Pond', 77),
+(5, 'Dean Winchester', 14),
+(6, 'Jodie Whittaker', 38);
+
+select * from owners;
+
+insert into species(id, name)
+values (1, 'Pokemon'),
+(2, 'Digimon');
+
+select * from species;
+
+update animals 
+set species_id = 1
+where name not like '%mon';
+
+update animals 
+set species_id = 2
+where name like '%mon';
+
+update animals
+set owner_id = 1
+where name = 'Agumon';
+
+update animals
+set owner_id = 2
+where name in ('Gabumon', 'Pikachu');
+
+update animals
+set owner_id = 3
+where name in ('Devimon', 'Plantmon');
+
+update animals
+set owner_id = 4
+where name in ('Charmander', 'Squirtle', 'Blossom');
+
+update animals
+set owner_id = 5
+where name in ('Angemon', 'Boarmon');
